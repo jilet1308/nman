9c9b5c550af50fb6983eb085a924a10af222abe7
jilet1308 <ismail.d.cokluk@gmail.com>
2026-02-18 09:32:40 +0300

    add the check to see if the renderer is available for use

--- src/com/jilet/NmanApplication.java
+++ src/com/jilet/NmanApplication.java
@@ -3,6 +3,7 @@ package com.jilet;
 
 import com.jilet.nman.cli.BaseArgs;
 import com.jilet.nman.service.ConfigurationService;
+import com.jilet.nman.service.RendererService;
 import picocli.CommandLine;
 
 public class NmanApplication {
@@ -10,6 +11,7 @@ public class NmanApplication {
     public static void main(String[] args) {
         ConfigurationService.createConfigIfNotPresent();
         ConfigurationService.createDefaultDocumentHomeIfNoneExists();
+        RendererService.checkRendererAvailable();
 
         new CommandLine(new BaseArgs()).execute(args);
     }
--- src/com/jilet/nman/service/RendererService.java
+++ src/com/jilet/nman/service/RendererService.java
@@ -9,6 +9,20 @@ public class RendererService {
     //Amazing cli application btw
     private static final String RENDERER_COMMAND = "glow";
 
+    public static void checkRendererAvailable() {
+        try {
+            int process = new ProcessBuilder(RENDERER_COMMAND, "--version")
+                    .inheritIO()
+                    .start()
+                    .waitFor();
+
+            if(process != 0) {
+                ExitUtil.exitWithErrorMessage("Renderer %s is not available. Please install it and try again.", RendererService.RENDERER_COMMAND);
+            }
+        } catch (IOException | InterruptedException e) {
+            ExitUtil.exitWithErrorMessage("Renderer %s is not available. Please install it and try again.", RendererService.RENDERER_COMMAND);
+        }
+    }
 
     public static void renderDocument(String name) {
         try {
